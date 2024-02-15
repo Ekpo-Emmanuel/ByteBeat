f8305cbd73c652871bc796894db1a17e78dd961e
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-02-15 14:51:20 -0800

    fixed routes logic

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,21 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const PostModel = require('./models/Posts')
 
+// Middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/posts')
+// Database connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Routes
 app.post('/api/posts', async (req, res) => {
     try {
         const { title, date, time, read, content, tags } = req.body;
-        const newPost = new PostModel({ title, date, time, read, content, tags });
-    
+        const newPost = new PostModel({ title, date, time, read, content, tags });    
         await newPost.save();
-    
+        
         res.status(201).json({ message: 'Post created successfully', post: newPost });
       } catch (error) {
         console.error('Error creating post:', error);
@@ -44,9 +46,7 @@ app.get('/', async (req, res) => {
 
 })
 
-
-
-
+// Start server
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
-import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
